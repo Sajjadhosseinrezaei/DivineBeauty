83c10b198bc794c3bc97a10421c99dd35f8ee639
sajjadhossein <sajjadhosseinrezaei@yahoo.com>
2025-04-18 00:50:51 +0330

    Refactor utility.py: reorganize imports and add OTPManager class with OTP handling functions

--- utility.py
+++ utility.py
@@ -1,5 +1,13 @@
 from django.shortcuts import redirect
 from django.utils.http import url_has_allowed_host_and_scheme
+from django.db import models
+from slugify import slugify
+import secrets
+import  bcrypt
+from django.core.mail import send_mail
+
+
+
 
 def redirect_with_next(request, default='/', param_name='next'):
     """
@@ -19,8 +27,6 @@ def redirect_with_next(request, default='/', param_name='next'):
     return redirect(default)
 
 
-from django.db import models
-from slugify import slugify
 
 class AutoSlugField(models.SlugField):
     """
@@ -35,4 +41,40 @@ class AutoSlugField(models.SlugField):
                 slug = slugify(base_value, allow_unicode=True) # ساخت اسلاگ فارسی
                 setattr(model_instance, self.attname, slug)
         return slug
+    
 
+
+
+class OTPManager:
+    @staticmethod
+    def generate_otp(length=6):
+        return ''.join(secrets.choice('0123456789') for _ in range(length))
+
+    @staticmethod
+    def hash_otp(otp):
+        return bcrypt.hashpw(otp.encode(), bcrypt.gensalt()).decode()  # خروجی string
+
+    @staticmethod
+    def verify_otp(otp, otp_hash):
+        return bcrypt.checkpw(otp.encode(), otp_hash.encode())  # هر دو bytes
+
+
+
+
+
+def send_otp_via_email(email, otp):
+    """
+    Sends the OTP to the user's email address.
+
+    Args:
+        email: The user's email address.
+        otp: The OTP to send.
+
+    Returns:
+        None
+    """
+    subject = 'Your OTP Code'
+    message = f'Your OTP code is: {otp}'
+    from_email = 'sajjadhosseinrezay6@gmail.com'  # Replace with your email address
+    recipient_list = [email] # List of recipient email addresses
+    send_mail(subject, message, from_email, recipient_list)
